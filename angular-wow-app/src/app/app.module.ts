f7c289b349f2791daee20c2ab584d6da101a230b
Simon Folger <s.folger@techdivision.com>
2017-10-08 13:39:22 +0200

    project basic setup

---
+++ angular-wow-app/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { TalentsComponent } from './talents/talents.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent },
+  {
+      path: 'talents',
+      component: TalentsComponent },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      component: PageNotFoundComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    TalentsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
